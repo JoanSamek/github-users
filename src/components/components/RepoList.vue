4c49899d2574c2236af36241c51d52869f5b459e
Rhegolie <rhegolie@gmail.com>
2022-05-11 22:47:11 +0200

    repo stats icons & loading

--- src/components/components/RepoList.vue
+++ src/components/components/RepoList.vue
@@ -1,20 +1,32 @@
 <template lang="">
-  <v-table density="compact">
-    <thead>
-      <tr>
-        <th class="text-left">Repository</th>
-        <th class="text-left">Statistics</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in repos" :key="item.id">
-        <td class="hoverUrl" @click="goToRepo(item.html_url)">
-          {{ item.name }}
-        </td>
-        <td></td>
-      </tr>
-    </tbody>
-  </v-table>
+  <div>
+    <div class="text-center" v-if="loading">
+      <v-progress-circular indeterminate color="primary" />
+    </div>
+    <v-table density="compact" v-else>
+      <thead>
+        <tr>
+          <th class="text-left">Repository</th>
+          <th class="text-right">Statistics</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in repos" :key="item.id">
+          <td class="hoverUrl" @click="goToRepo(item.html_url)">
+            {{ item.name }}
+          </td>
+          <td class="text-right">
+            <span v-for="(statData, statName) in stats" :key="statName">
+              <v-icon large :color="statData.color" class="ml-6">
+                {{ statData.icon }}
+              </v-icon>
+              {{ statName }}: {{ statData.count }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 <script>
 import api from "../../services/api.js";
@@ -22,7 +34,25 @@ export default {
   name: "RepoList",
   props: ["user"],
   data() {
-    return { loading: false, repos: [] };
+    return {
+      loading: true,
+      repos: [],
+      stats: {
+        stars: { icon: "mdi-star", color: "orange", count: 0 },
+        watchers: { icon: "mdi-eye", color: "blue", count: 0 },
+        comments: { icon: "mdi-comment", color: "green", count: 0 },
+        commits: {
+          icon: "mdi-source-commit",
+          color: "purple",
+          count: 0,
+        },
+        open_issues: {
+          icon: "mdi-alert-circle-outline",
+          color: "red",
+          count: 0,
+        },
+      },
+    };
   },
   methods: {
     goToRepo(url) {
